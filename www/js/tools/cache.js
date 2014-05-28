37a323890f59ff158b791d81fcf507480cadcfc7
Antoine Jackson <a.j.william26@gmail.com>
2014-05-28 20:29:30 +0200

    replaced old oauth.js lib with newer version with promises and me

---
+++ www/js/tools/cache.js
@@ -0,0 +1,43 @@
+module.exports = {
+  init: function(cookies_module, config) {
+    this.config = config;
+    return this.cookies = cookies_module;
+  },
+  tryCache: function(OAuth, provider, cache) {
+    var e, i, res;
+    if (this.cacheEnabled(cache)) {
+      cache = this.cookies.readCookie("oauthio_provider_" + provider);
+      if (!cache) {
+        return false;
+      }
+      cache = decodeURIComponent(cache);
+    }
+    if (typeof cache === "string") {
+      try {
+        cache = JSON.parse(cache);
+      } catch (_error) {
+        e = _error;
+        return false;
+      }
+    }
+    if (typeof cache === "object") {
+      res = {};
+      for (i in cache) {
+        if (i !== "request" && typeof cache[i] !== "function") {
+          res[i] = cache[i];
+        }
+      }
+      return OAuth.create(provider, res, cache.request);
+    }
+    return false;
+  },
+  storeCache: function(provider, cache) {
+    this.cookies.createCookie("oauthio_provider_" + provider, encodeURIComponent(JSON.stringify(cache)), cache.expires_in - 10 || 3600);
+  },
+  cacheEnabled: function(cache) {
+    if (typeof cache === "undefined") {
+      return this.config.options.cache;
+    }
+    return cache;
+  }
+};
